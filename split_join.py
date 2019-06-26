3cc08900e232bc565db74226f23723990b8ce85d
unknown <mdgolamrasulshadin0@gmail.com>
2019-06-26 23:26:09 +0600

    method////

---
+++ split_join.py
@@ -0,0 +1,9 @@
+# usr_info='harsit.34'.split('.')
+# print(usr_info)
+# name,age=input("enter your name and age").split(',')
+# print(name)
+# print(age)
+
+#join method
+usr_info=['karim','34']
+print(','.join(usr_info))
